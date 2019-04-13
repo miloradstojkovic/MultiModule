e4576d5e93f98f36174ef7bdf8539b912f74bdd1
mstojkovic <mstojkovic@emisia.net>
2019-04-13 20:49:57 +0200

    d

---
+++ model/src/main/java/com/milorad/mod/model/Customer.java
@@ -0,0 +1,14 @@
+package com.milorad.mod.model;
+
+public class Customer {
+
+    private String customer;
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+}
---
+++ service/src/test/java/com/milorad/mod/service/TempTest.java
@@ -0,0 +1,11 @@
+package com.milorad.mod.service;
+
+import com.milorad.mod.model.Customer;
+
+public class TempTest {
+    public static void main(String[] args) {
+       // test method
+
+
+    }
+}
