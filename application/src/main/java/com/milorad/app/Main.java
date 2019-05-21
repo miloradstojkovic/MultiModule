28be06b35288c7b49da10a0ed43d0b7a952cc134
mstojkovic <mstojkovic@emisia.net>
2019-05-21 18:26:52 +0200

    first dev change

---
+++ application/src/main/java/com/milorad/app/Main.java
@@ -0,0 +1,41 @@
+package com.milorad.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication()
+@Configuration
+@EnableAutoConfiguration(exclude = { DataSourceAutoConfiguration.class, HibernateJpaAutoConfiguration.class })
+@ImportResource("classpath:app-config.xml")
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+        System.out.println("http://localhost:8777/ping?testCase=someTestCase");
+        System.out.println("http://localhost:8777/getLog");
+        System.out.println("http://localhost:8777/clearLog");
+
+
+    }
+
+
+    @Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+
+        TomcatEmbeddedServletContainerFactory factory = new TomcatEmbeddedServletContainerFactory();
+
+       // TomcatContextCustomizer contextCustomizer = context -> context.addWelcomeFile("index.gs4tr");
+       // factory.addContextCustomizers(contextCustomizer);
+
+        return factory;
+    }
+
+}
